b77006728adb214abddb4c6958ac2e4bfea8b811
konstantin <kos.krylov@mail.ru>
2018-11-17 21:37:09 +0300

    fixed a few bugs

--- src/com/epam/gamedev/Game.java
+++ src/com/epam/gamedev/Game.java
@@ -7,7 +7,7 @@ public class Game {
 
     private GameField field;
     private boolean isCurrentX = true;
-    private final Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in); //нужно ли где то scanner.close()????
 
     public Game() {
         field = new GameField();
@@ -22,7 +22,7 @@ public class Game {
             gameNotFinish = field.checkForWinner(isCurrentX);
             isCurrentX = !isCurrentX;
         }
-        boolean oneMoreTime = restart();
+        boolean oneMoreTime = isRestart();
         if (oneMoreTime) {
             new Game();
         }
@@ -30,7 +30,7 @@ public class Game {
 
 
     void playerMove() {
-        boolean validInput = false;  // for input validation
+        boolean validInput = false;
         do {
             if (isCurrentX) {
                 System.out.print("Player 'X', enter your move (row[1-3] column[1-3]): ");
@@ -39,29 +39,31 @@ public class Game {
             }
             int row = scanner.nextInt() - 1;  // array index starts at 0 instead of 1
             int col = scanner.nextInt() - 1;
-            if (row >= 0 && row < GameField.ROWS && col >= 0 && col < GameField.COLS && field.getField()[row][col] == '-') {
+            if (row >= 0 && row < GameField.ROWS && col >= 0 && col < GameField.COLS &&
+                    field.getField()[row][col] == '-') {
                 field.currentRow = row;
                 field.currentCol = col;
-                validInput = true;  // input okay, exit loop
+                field.addMoveCount(); // нужен ли такой метод или увеличивать значение количества ходов по другому???
+                validInput = true;
             } else {
                 System.out.println("This move at (" + (row + 1) + "," + (col + 1)
                         + ") is not valid. Try again...");
             }
-        } while (!validInput);  // repeat until input is valid
+        } while (!validInput);
     }
 
-    boolean restart() {
+    boolean isRestart() {
         System.out.println("One more time? If 'YES' enter 1, if 'NO' enter 2");
         int answer = scanner.nextInt();
         switch (answer) {
             case 1:
-                System.out.println("Lets Go");
+                System.out.println("Let's Play!");
                 return true;
             case 2:
                 System.out.println("Bye!");
                 return false;
             default:
-                System.out.println("Enter 1 or 2");
+                System.out.println("Enter 1 or 2"); //как сделать, что бы выбор повторялся, если значение не 1 или 2???
                 return false;
         }
     }
--- src/com/epam/gamedev/GameField.java
+++ src/com/epam/gamedev/GameField.java
@@ -5,6 +5,7 @@ public class GameField {
 
     private char[][] field;
     int currentRow, currentCol;  // the current row and column
+    private int moveCount;
 
     public GameField() {
         field = new char[ROWS][COLS];
@@ -18,6 +19,10 @@ public class GameField {
 
     public char[][] getField() {
         return field;
+    }
+
+    public void addMoveCount() {
+        this.moveCount++;
     }
 
     void printField() {
@@ -39,64 +44,37 @@ public class GameField {
     }
 
     boolean checkForWinner(boolean isCurrentX) {
-        for (int i = 0; i < ROWS; i++) {
-            // checking horizontal / vertical
-            if (((field[i][0] == field[i][1] && field[i][0] == field[i][2]) ||
-                    (field[0][i] == field[1][i] && field[0][i] == field[2][i])) && field[i][i] != '-') {
+        if (moveCount >= 5) {
+            for (int i = 0; i < ROWS; i++) {
+                // checking horizontal / vertical
+                if (((field[i][0] == field[i][1] && field[i][0] == field[i][2]) ||
+                        (field[0][i] == field[1][i] && field[0][i] == field[2][i])) && field[i][i] != '-') {
+                    System.out.println("Player " + (isCurrentX ? "'X'" : "'O'") + " won!");
+                    return false;
+                }
+            }
+            // checking diagonals
+            if (((field[0][0] == field[1][1] && field[0][0] == field[2][2]) && field[0][0] != '-' ||
+                    (field[0][2] == field[1][1] && field[0][2] == field[2][0])) && field[0][2] != '-') {
                 System.out.println("Player " + (isCurrentX ? "'X'" : "'O'") + " won!");
                 return false;
             }
-        }
-//        if (((field[0][0] == field[1][1] && field[0][0] == field[2][2]) && field[0][0] != '-' ||
-//                (field[0][2] == field[1][1] && field[0][2] == field[2][0])) && field[0][2] != '-') {
-//            System.out.println("Player " + (isCurrentX ? "'X'" : "'O'") + " won!");
-//            return false;
-//        }
 
-        if (currentCol == currentRow) {
-            //we're on a diagonal
-            for (int i = 0; i < ROWS; i++) {
-                if (field[i][i] == '-') {
-                    break;
-                }
-
-            }
+//            if (currentCol == currentRow) { // возможно реализовать более интересную реализацию проверки диагоналей
+//                //we're on a diagonal
+//                for (int i = 0; i < ROWS; i++) {
+//                    if (field[i][i] == '-') {
+//                        break;
+//                    }
+//                    if (field[i][i] == field[i + 1][i + 1] && field[i][i] == field[i + 2][i + 2]) {
+//                        System.out.println("Player " + (isCurrentX ? "'X'" : "'O'") + " won!");
+//                        return false;
+//                    }
+//                }
+//            }
         }
         return true;
     }
-
-    //check diag
-//    boolean checkDiagonal(boolean isCurrentX) {
-//        if (currentCol == currentRow) {
-//            //we're on a diagonal
-//            for (int i = 0; i < 3; i++) {
-//                if (field[i][i] == '-') {
-//                    break;
-//                }
-//                else {
-//                    System.out.println("Player " + (isCurrentX ? "'X'" : "'O'") + " won!");
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
-//    }
-//
-//    //check anti diag (thanks rampion)
-//        if(x + y == n - 1){
-//        for(int i = 0; i < n; i++){
-//            if(field[i][(n-1)-i] != s)
-//                break;
-//            if(i == n-1){
-//                //report win for s
-//            }
-//        }
-//    }
-//
-//    //check draw
-//        if(moveCount == (Math.pow(n, 2) - 1)){
-//        //report draw
-//    }
-
-
 }
+
+
